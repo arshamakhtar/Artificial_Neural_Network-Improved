7d8a3255b27c29fa6be341ec7f1bad79c0c85c09
arshamakhtar <arshamakhtar123@gmail.com>
2025-08-07 11:23:11 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector> 
+#include <stdio.h>
+#include <fstream>
+#include <streambuf>
+#include <ostream>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    cout<<"Working";
+    return 0;
+}
